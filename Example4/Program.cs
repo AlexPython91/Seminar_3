f272b9c5a5edeb678e799aadee10dfcbe0f33dbb
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-04 23:32:05 +0400

    added Example 4

---
+++ Example4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// Найти максимальное из трех чисел
+
+int a = 2;
+int b = 3;
+int c = 5;
+int max = a;
+
+if(b > max) max = b;
+if(c > max) max = c;
+else max = a;
+
+Console.WriteLine("Максимальное число = " + max);
