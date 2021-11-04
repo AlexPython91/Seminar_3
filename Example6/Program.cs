02b37be0c0d1331b08cdd8a8d30c755f05124b4b
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-04 23:32:44 +0400

    added Example 6

---
+++ Example6/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// Выяснить является ли число чётным
+
+int number = 4;
+if(number % 2 == 0)
+{
+    Console.WriteLine("Число " + number + " является четным!");
+}
+else
+    Console.WriteLine("Число " + number + " нечетное");
