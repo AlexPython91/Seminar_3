0466e5966eb12749fc7b62d4552a543aaa04b83a
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-04 23:31:35 +0400

    added Example 2

---
+++ Example2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+// Даны два числа. Показать большее и меньшее число
+
+int numberOne = 2;
+int numberTwo = 8;
+int max = numberOne;
+
+if(numberOne > numberTwo) 
+{
+    max = numberOne;
+    Console.WriteLine("Максимальное число = " + numberOne); 
+    Console.WriteLine("Минимальное число = " + numberTwo);
+}
+else
+    max = numberTwo;
+    Console.WriteLine("Максимальное число = " + numberTwo);
+    Console.WriteLine("Минимальное число = " + numberOne);
+   
