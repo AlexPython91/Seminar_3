1820a59c3a99af5771260a86c6aa13f54cc9b8be
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-05 21:53:48 +0400

    added Example 1

---
+++ Example1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// По двум введённым числам проверять является ли первое квадратом второго
+
+int numberOne = 2;
+int numberTwo = 4;
+//int square = numberOne * numberOne;
+
+if(numberTwo == numberOne * numberOne)
+{
+    Console.WriteLine(numberTwo + " is the square of number 2");
+}
+else 
+{
+    Console.WriteLine(numberTwo + " is not the square of number 2");
+}
