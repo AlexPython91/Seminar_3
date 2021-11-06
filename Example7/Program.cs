08b2efcbbcb5818d1d58c74639f099697be101c2
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-06 19:12:32 +0400

    added Example 7

---
+++ Example7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// Показать числа от -N до N
+
+int[] array = new int[10];   // создаем массив из 10 значений
+
+void createArray(int[] array)    // заполняем массив случ. числами в диапазоне с пом. метода
+{
+    for(int i = 0; i < 10; i++)
+    {
+        array[i] = new Random().Next(-10, 11);
+    }
+}
+
+void showArray(int[] array)     // создаем метод вывода массива в консоль
+{
+    for(int j = 0; j < array.Length; j++)
+    {
+        Console.Write($"{array[j]} ");
+    }
+}
+
+createArray(array); 
+Console.WriteLine("Показ всех чисел массива от -N до N: ");  // вызываем оба метода
+showArray(array);
+Console.WriteLine();
