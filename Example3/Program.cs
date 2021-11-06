10720919e61f985ad026f8c6189e7823167e86e0
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-06 15:22:03 +0400

    added Example3

---
+++ Example3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// По введенному номеру дня недели вывести его название
+
+// string[] daysOfTheWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+// Console.Write("Enter a number from one to seven: ");
+// int daysOfWeek = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine(daysOfTheWeek[daysOfWeek - 1]);
+
+
+
+
+
+
+
+
