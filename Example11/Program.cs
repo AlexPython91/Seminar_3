c10b4e9100051839ae4fdcd667b3870f995ab8e7
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-07 09:35:34 +0400

    added Example 11

---
+++ Example11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 99);                         // Задаем случ. двузначное число
+Console.WriteLine("Given in significant number: " + number);   // Вывод в терминал
+
+int firstDigit = number / 10;    // Полученное число делим на 10
+int secondDigit = number % 10;        // от полученного числа
+
+if(firstDigit > secondDigit)     // Сравниваем деленное число с отсатком от деленного числа
+{
+    Console.WriteLine("The first digit is greater");       // Вывод результата сравнения
+}
+else
+    Console.WriteLine("The second digit is greater");
+
