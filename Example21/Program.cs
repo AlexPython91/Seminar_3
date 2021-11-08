7c9f9a499774c182d1bc283cda01ded512c6679a
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-08 23:31:37 +0400

    added Example 21

---
+++ Example21/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Программа проверяет пятизначное число на палиндромом.
+
+int a = 12321;
+int b = a / 10000;
+int c = a % 10;
+int d = a % 10000 / 1000;
+int e = a % 100 / 10;
+if(b == c && d == e) 
+{
+    Console.WriteLine("Полиндром");
+}
+else
+    Console.WriteLine("Не полиндром");
+
