c3154d37f5a0e8ff78071fda5c01365e2d698dbd
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-09 22:26:24 +0400

    decided by method

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -2,18 +2,28 @@
 
 // Даны два числа. Показать большее и меньшее число
 
-int numberOne = 2;
-int numberTwo = 8;
-int max = numberOne;
+// int numberOne = 2;
+// int numberTwo = 8;
+// int max = numberOne;
 
-if(numberOne > numberTwo) 
-{
-    max = numberOne;
-    Console.WriteLine("Максимальное число = " + numberOne); 
-    Console.WriteLine("Минимальное число = " + numberTwo);
-}
-else
-    max = numberTwo;
-    Console.WriteLine("Максимальное число = " + numberTwo);
-    Console.WriteLine("Минимальное число = " + numberOne);
+// if(numberOne > numberTwo) 
+// {
+//     max = numberOne;
+//     Console.WriteLine("Максимальное число = " + numberOne); 
+//     Console.WriteLine("Минимальное число = " + numberTwo);
+// }
+// else
+//     max = numberTwo;
+//     Console.WriteLine("Максимальное число = " + numberTwo);
+//     Console.WriteLine("Минимальное число = " + numberOne);
    
+
+   int findMaximumNumber(int a, int b)
+   {
+       if(a > b)
+       {
+           return a;
+       }
+       else return b;
+   }
+   Console.WriteLine(findMaximumNumber(1, 13));
