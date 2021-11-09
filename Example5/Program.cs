e25065bb36440fc393ea2e2fb003f0542d144fb8
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-09 22:44:33 +0400

    added solution by method

--- Example5/Program.cs
+++ Example5/Program.cs
@@ -2,6 +2,17 @@
 
 // Написать программу вычисления значения функции y=f(a)
 
-int f = 6;    // взяли произвольное число
-int y = f * f - 5;    // расчет по формуле 
-Console.WriteLine(y);
+// int f = 6;    // взяли произвольное число
+// int y = f * f - 5;    // расчет по формуле 
+// Console.WriteLine(y);
+
+// Второй вариант методом
+
+int f = 3;
+
+int findValue(int f)
+{
+    //int y = f * f - 5;
+    return (f * f) - 5;
+}
+Console.WriteLine(findValue(f));
