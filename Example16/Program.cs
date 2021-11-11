439fb2c07ff9b5cecba5604aab6be377c41b7448
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-12 00:46:54 +0400

    added Example 16

---
+++ Example16/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// 16. Дано число обозначающее день недели. Выяснить является номер недели выходным днём
+
+Console.Write("Please enter a number from 1 to 7: ");
+string c = Console.ReadLine();
+
+bool dayOff(string a)
+{
+    return (a == "6" | a == "7");
+}
+Console.WriteLine(dayOff(c));
