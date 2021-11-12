403d6d0601946ce0230c2225d8da0fbbbf55d5e8
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-12 21:08:01 +0400

    added Example 28

---
+++ Example28/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// 28 Подсчитать сумму цифр в числе
+
+int num = 6524;
+int result = 0; 
+
+while(num > 0)  // num = 123      //12        // 1
+{
+    //int result = 0;  // result = 0     //0       //0
+    result = result + num % 10;  // result = 0 + 3       // result = 0 + 2   // result = 0 + 1
+    num = num / 10;           // num = 12            // num = 1     // num = 0
+    //Console.WriteLine(result);   // 3       // 2       // 1
+}
+Console.WriteLine(result);   // 3       // 2       // 1
+// 123 
