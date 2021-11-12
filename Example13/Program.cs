92c4233a856d8c35da0fa11ca65505f90f66c0f9
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-12 20:26:14 +0400

    added Example 13 and the version executed by the method

---
+++ Example13/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+// 13. Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+
+int number = 80;                          // Задаем число
+Console.WriteLine("Number = " + number);  // Выводим в консоль
+
+int givenNumber = 13;                               // Задаем число
+Console.WriteLine("GivenNumber = " + givenNumber); // Выводим в консоль
+
+if (number % givenNumber == 0)         // Вычисляем остаток от деления чисел
+{
+    Console.WriteLine("Кратное!");
+}
+else
+{
+    Console.WriteLine("Остаток от числа = " + number % givenNumber);
+}
+
+int Show(int a, int b)
+{
+    return (a % b);
+}
+int res = Show(80, 20);
+Console.WriteLine(res);
