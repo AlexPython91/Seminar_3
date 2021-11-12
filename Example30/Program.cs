9bb8e07241fdd8582cf6735bdb7073e399bf5496
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-12 21:38:22 +0400

    added Example 30

---
+++ Example30/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// 30. Показать кубы чисел, заканчивающихся на четную цифру 
+
+int number = 1;
+int count = 10;
+
+for(number = 1; number <= count; number++)     // Задаем цикл по вход. данным
+{   
+    int sqr = number*number*number;            // Находим куб каждого числа
+    if(sqr % 2 == 0)                           // Производим расчет остатка от деления (четные)
+    {
+        Console.WriteLine($"Ends in even: {sqr} ", "");
+    }
+}
