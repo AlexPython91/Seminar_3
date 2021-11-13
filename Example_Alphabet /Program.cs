fbf525762826df71f988f3ef32beee525b24132c
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-13 22:16:25 +0400

    added program alphabet

---
+++ Example_Alphabet /Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+// Вывести на экран ангийский алфавит
+// Используем символьную арифметику + цикл
+
+char glif = 'a';
+
+while(glif <= 'z')
+{
+    Console.WriteLine(glif);
+    glif++;
+}
