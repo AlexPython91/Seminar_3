fd3f7a31630329b83ab0df0b50f8e924fa460844
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-15 00:07:38 +0400

    added Example 31

---
+++ Example31/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+// 31. Задать массив из 8 элементов и вывести их на экран 
+
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(1, 30);
+    Console.Write(array[i] + " ");
+}
