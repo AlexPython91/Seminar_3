5f728377b335a31cdeaa664960c5f3f8d3064978
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-16 00:16:29 +0400

    added Example 35

---
+++ Example35/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+// 35. Определить, присутствует ли в заданном массиве, некоторое число
+
+int[] array = new int[14];
+for (int i = 0; i < 14; i++)
+{
+    array[i] = new Random().Next(1, 14);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int N = 5;
+int count = 0;
+for (int i = 0; i < 14; i++)
+{
+    if (array[i] == N)
+    {
+        count = 1;
+    }
+
+}
+if (count > 0)
+{
+    Console.WriteLine("Число " + N + " присутствует " + count + " раз");
+}
+else
+{
+    Console.WriteLine("Число отсутствует"!);
+}
+
+// int N;
+// int count = 0;
+// bool FindeNum(int[] array)
+// {
+//     for (int i = 0; i < 14; i++)
+//     {
+//         if (array[i] == N)
+//         {
+//             count++;
+//         }
+//     }
+// }
+// FindeNum(5);
