a814ba874ea9db012a50306536fb8434e55a80fa
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-17 22:49:01 +0400

    added Example 34

---
+++ Example23/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// 23. Показать таблицу квадратов чисел от 1 до N
+
+int[] array = new int[2];
+//Console.Clear();
+Console.WriteLine("Числа от 1 до N: ");
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 15);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+
+int result = 1;
+for(int i = 0; i < array.Length; i++)
+{
+    result = result * array[i];
+}
+Console.WriteLine("Таблица квадратов: " + result);
+
+
+
+
+// int[] getArraySqr(int size)
+// {
+//     int[] result = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         result[i] = i * i;
+//     }
+//     return result;
+// }
+// PrintArray(getArraySqr(8));
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         //array[0] = 1;
+//         Console.Write(array[i] + " ");
+//     }
+// }
+
---
+++ Example34/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// 34. Написать программу замену элементов массива на противоположные
+
+int[] array = new int[5];
+Console.Clear();
+Console.WriteLine("Программа замены элементов массива на противоположные");
+Console.WriteLine("Список элементов: ");
+for (int i = 0; i < array.Length; i++)        // Цикл заполнения массива
+{
+    array[i] = new Random().Next(1, 20);
+    Console.Write(array[i] + " ");        // Вывод массива
+}
+Console.WriteLine();
+
+Console.WriteLine("Список противоположных элементов: ");
+int result = 0;                            // Задаем начальное значение
+for (int i = 0; i < array.Length; i++)     // Цикл прохождения по массиву
+{
+    if (array[i] > 0)            // Если элемент массива больше 0
+    {
+        result = array[i] = -array[i];    // Присваиваем ему противоположное значение
+        Console.Write(result + " ");
+    }
+}
+Console.WriteLine();
+
+
