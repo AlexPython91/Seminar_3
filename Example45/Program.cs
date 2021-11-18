fd0bd49756eb755a304de368d18db4f253df7a6d
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-19 00:01:53 +0400

    added Example 45

---
+++ Example45/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// 45. Показать числа Фибоначчи
+
+int firstFib = 0;
+int secondFib = 1;
+int n = 5;
+
+for (int i = 0; i < n; i++)
+{
+    secondFib = secondFib + firstFib;
+    firstFib = secondFib - firstFib;
+    Console.Write(secondFib + " ");
+}
