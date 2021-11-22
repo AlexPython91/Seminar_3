55fd0b71ef4fb6c7d45f5d55600e0cb0584f0733
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-22 23:32:28 +0400

    added Example 48

---
+++ Example48/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+int[,] CreateTwoDemensArray = new int[3, 5];
+
+for (int i = 0; i < CreateTwoDemensArray.GetLength(0); i++)
+{
+    for (int j = 0; j < CreateTwoDemensArray.GetLength(1); j++)
+    {
+        Console.Write($"{CreateTwoDemensArray[i, j]}" + " ");
+    }
+    Console.WriteLine();
+}
+
+
+
+
