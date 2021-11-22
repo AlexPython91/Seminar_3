a222a61938b6ac3af164393190de7b88cd84fa14
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-22 23:44:36 +0400

    added Example 43

---
+++ Example43/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+// 43. Написать программу преобразования десятичного числа в двоичное
+
+Console.Write("Введите число в десятичной системе: ");
+
+string ToBinary(int decimalNumber)
+{
+    var binaryNumber = string.Empty;
+    while (decimalNumber > 0)
+    {
+        binaryNumber = (decimalNumber % 2) + binaryNumber;
+        decimalNumber /= 2;
+    }
+    return binaryNumber;
+}
+
+var decNum = int.Parse(Console.ReadLine());
+var binNum = ToBinary(decNum);
+Console.WriteLine("Двоичное число = {1}", decNum, binNum);
