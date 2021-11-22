06617ab2ea52a2a0c502e36c8d62d84b9a8b4afc
alexhvorostov <hvorostov.alex1@gmail.com>
2021-11-22 06:15:50 +0400

    fixed a logical error. May be)

--- Example37/Program.cs
+++ Example37/Program.cs
@@ -2,21 +2,18 @@
 
 // 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
 
-int[] array = new int[123];
+Console.WriteLine("Задан одномерный массив из 123 элементов: ");
+int[] array = new int[124];
 for (int i = 0; i < 122; i++)
 {
-    array[i] = new Random().Next(0, 20);
+    array[i] = new Random().Next(1, 123);
     Console.Write(array[i] + " ");       
 }
 int count = 0;
 for (int i = 0; i < 122; i++)
-    if (array[i] >= 9 & array[i] < 100)
+    if (array[i] >= 9 && array[i] <= 99)
     {
         count++;
     }
 Console.WriteLine();
-Console.WriteLine("++++++++++++++");
-Console.WriteLine(count);
-
-
-
+Console.WriteLine($"Колличество элементов из отрезка {10} {99} = {count}");
