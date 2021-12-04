1f779aac274ea31fae7063496fa6047d1ab2ec9c
alexhvorostov <hvorostov.alex1@gmail.com>
2021-12-04 21:39:14 +0400

    added Example 40

---
+++ Example40/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+// 40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом 
+
+double[] RealArray = new double[5];
+double min = 0;
+double max = 0;
+double R;
+
+for(int i = 0; i < RealArray.Length; i++)
+{
+    Console.Write("Введите {0}-е число: ", i + 1);
+    R = double.Parse(Console.ReadLine());
+
+    if(R > max && R < min) max = R; min = R;
+}
+Console.WriteLine("Разница между числами = {0}", max - min);
+
